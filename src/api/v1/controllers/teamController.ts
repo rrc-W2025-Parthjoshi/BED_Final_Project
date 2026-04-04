710d3f2c4275afde5ee64bcc128046f0055878ef
Parth Joshi <pjoshi32@academic.RRC.CA>
2026-04-03 22:22:23 -0500

    Added team controller file

---
+++ src/api/v1/controllers/teamController.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from "express";
+import * as teamService from "../services/teamService";
+
+/**
+ * Get all teams
+ * @param req - Express request object
+ * @param res - Express response object
+ */
+export const getAllTeams = (req: Request, res: Response): void => {
+    const teams = teamService.getAllTeams();
+    res.status(200).json({ message: "Get all teams", data: teams });
+};
+
+/**
+ * Get a single team by ID
+ * @param req - Express request object
+ * @param res - Express response object
+ */
+export const getTeamById = (req: Request, res: Response): void => {
+    const { id } = req.params;
+    const team = teamService.getTeamById(id);
+    res.status(200).json({ message: "Get team", data: team });
+};
+
+/**
+ * Create a new team
+ * @param req - Express request object
+ * @param res - Express response object
+ */
+export const createTeam = (req: Request, res: Response): void => {
+    const newTeam = teamService.createTeam(req.body);
+    res.status(201).json({ message: "Team created", data: newTeam });
+};
+
+/**
+ * Update a team
+ * @param req - Express request object
+ * @param res - Express response object
+ */
+export const updateTeam = (req: Request, res: Response): void => {
+    const { id } = req.params;
+    const updatedTeam = teamService.updateTeam(id, req.body);
+    res.status(200).json({ message: "Team updated", data: updatedTeam });
+};
+
+/**
+ * Delete a team
+ * @param req - Express request object
+ * @param res - Express response object
+ */
+export const deleteTeam = (req: Request, res: Response): void => {
+    const { id } = req.params;
+    teamService.deleteTeam(id);
+    res.status(200).json({ message: "Team deleted" });
+};
