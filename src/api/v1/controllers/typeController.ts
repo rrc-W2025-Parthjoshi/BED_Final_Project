92c4533feaad0914ab5e34974681db34786960c0
Parth Joshi <pjoshi32@academic.RRC.CA>
2026-04-04 00:46:04 -0500

    Updated controllers

--- src/api/v1/controllers/typeController.ts
+++ src/api/v1/controllers/typeController.ts
@@ -1,56 +1,82 @@
-import { Request, Response } from "express";
-import { HTTP_STATUS } from "../../../constants/httpsConstants";
+import { Request, Response, NextFunction } from "express";
 import * as typeService from "../services/typeService";
+import { successResponse } from "../models/responseModel";
+import { HTTP_STATUS } from "../../../constants/httpsConstants";
 
 /**
  * Get all types
  * @param req - Express request object
  * @param res - Express response object
+ * @param next - Express next function
  */
-export const getAllTypes = (req: Request, res: Response): void => {
-    const types = typeService.getAllTypes();
-    res.status(HTTP_STATUS.OK).json({ message: "Get all types", data: types });
+export const getAllTypes = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const types = await typeService.getAllTypes();
+        res.status(HTTP_STATUS.OK).json(successResponse(types, "Get all types"));
+    } catch (error: unknown) {
+        next(error);
+    }
 };
 
 /**
  * Get a single type by ID
  * @param req - Express request object
  * @param res - Express response object
+ * @param next - Express next function
  */
-export const getTypeById = (req: Request, res: Response): void => {
-    const { id } = req.params;
-    const type = typeService.getTypeById(id);
-    res.status(HTTP_STATUS.OK).json({ message: "Get type", data: type });
+export const getTypeById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const { id } = req.params;
+        const type = await typeService.getTypeById(id);
+        res.status(HTTP_STATUS.OK).json(successResponse(type, "Get type"));
+    } catch (error: unknown) {
+        next(error);
+    }
 };
 
 /**
  * Create a new type
  * @param req - Express request object
  * @param res - Express response object
+ * @param next - Express next function
  */
-export const createType = (req: Request, res: Response): void => {
-    const newType = typeService.createType(req.body);
-    res.status(HTTP_STATUS.CREATED).json({ message: "Type created", data: newType });
+export const createType = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const typeId = await typeService.createType(req.body);
+        res.status(HTTP_STATUS.CREATED).json(successResponse({ id: typeId }, "Type created"));
+    } catch (error: unknown) {
+        next(error);
+    }
 };
 
 /**
  * Update a type
  * @param req - Express request object
  * @param res - Express response object
+ * @param next - Express next function
  */
-export const updateType = (req: Request, res: Response): void => {
-    const { id } = req.params;
-    const updatedType = typeService.updateType(id, req.body);
-    res.status(HTTP_STATUS.OK).json({ message: "Type updated", data: updatedType });
+export const updateType = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const { id } = req.params;
+        await typeService.updateType(id, req.body);
+        res.status(HTTP_STATUS.OK).json(successResponse({}, "Type updated"));
+    } catch (error: unknown) {
+        next(error);
+    }
 };
 
 /**
  * Delete a type
  * @param req - Express request object
  * @param res - Express response object
+ * @param next - Express next function
  */
-export const deleteType = (req: Request, res: Response): void => {
-    const { id } = req.params;
-    typeService.deleteType(id);
-    res.status(HTTP_STATUS.OK).json({ message: "Type deleted" });
+export const deleteType = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const { id } = req.params;
+        await typeService.deleteType(id);
+        res.status(HTTP_STATUS.OK).json(successResponse({}, "Type deleted"));
+    } catch (error: unknown) {
+        next(error);
+    }
 };
