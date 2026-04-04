50d5663a749200b72eb387d349c8d76ce93357e5
Parth Joshi <pjoshi32@academic.RRC.CA>
2026-04-03 22:35:52 -0500

    Updated App.ts with all routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, { Express } from "express";
-import healthRoutes from "./api/v1/routes/healthroutes";
+import healthRoutes from "./api/v1/routes/healthRoutes";
+import pokemonRoutes from "./api/v1/routes/pokemonRoutes";
+import typeRoutes from "./api/v1/routes/typeRoutes";
+import teamRoutes from "./api/v1/routes/teamRoutes";
 
 // Initialize Express application
 const app: Express = express();
@@ -8,5 +11,8 @@ app.use(express.json());
 
 // Routes
 app.use("/api/v1", healthRoutes);
+app.use("/api/v1", pokemonRoutes);
+app.use("/api/v1", typeRoutes);
+app.use("/api/v1", teamRoutes);
 
 export default app;
