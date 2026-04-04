43ef143f68f9f5d650b1b61f07f81da1a77e3ce3
Parth Joshi <pjoshi32@academic.RRC.CA>
2026-04-04 00:30:26 -0500

    Added Type validation schemas

---
+++ src/api/v1/validation/typeSchemas.ts
@@ -0,0 +1,45 @@
+import Joi from "joi";
+
+export const typeSchemas = {
+    // POST /types - Create new type
+    create: {
+        body: Joi.object({
+            name: Joi.string().required().messages({
+                "any.required": "Type name is required",
+                "string.empty": "Type name cannot be empty",
+            }),
+        }),
+    },
+    // GET /types/:id - Get single type
+    getById: {
+        params: Joi.object({
+            id: Joi.string().required().messages({
+                "any.required": "Type ID is required",
+                "string.empty": "Type ID cannot be empty",
+            }),
+        }),
+    },
+    // PUT /types/:id - Update type
+    update: {
+        params: Joi.object({
+            id: Joi.string().required().messages({
+                "any.required": "Type ID is required",
+                "string.empty": "Type ID cannot be empty",
+            }),
+        }),
+        body: Joi.object({
+            name: Joi.string().optional().messages({
+                "string.empty": "Type name cannot be empty",
+            }),
+        }),
+    },
+    // DELETE /types/:id - Delete type
+    delete: {
+        params: Joi.object({
+            id: Joi.string().required().messages({
+                "any.required": "Type ID is required",
+                "string.empty": "Type ID cannot be empty",
+            }),
+        }),
+    },
+};
