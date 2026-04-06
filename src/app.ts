12bfe4bdb7bb25908a305faafc55478cd052e1ef
Parth Joshi <pjoshi32@academic.RRC.CA>
2026-04-06 17:19:52 -0500

    Added New Component to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import morgan from "morgan";
+import rateLimit from "express-rate-limit";
 import healthRoutes from "./api/v1/routes/healthRoutes";
 import pokemonRoutes from "./api/v1/routes/pokemonRoutes";
 import typeRoutes from "./api/v1/routes/typeRoutes";
@@ -8,6 +9,13 @@ import teamRoutes from "./api/v1/routes/teamRoutes";
 // Initialize Express application
 const app: Express = express();
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests, please try again later.",
+});
+
+app.use(limiter);
 app.use(morgan("combined"));
 app.use(express.json());
 
