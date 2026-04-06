36ba5a1ed3db47bcb564c04c54a4afef1d41a672
Parth Joshi <pjoshi32@academic.RRC.CA>
2026-04-06 18:00:15 -0500

    Added custom error classes

--- src/api/v1/controllers/pokemonController.ts
+++ src/api/v1/controllers/pokemonController.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import * as pokemonService from "../services/pokemonService";
 import { successResponse } from "../models/responseModel";
-import { HTTP_STATUS } from "../../../constants/httpsConstants";
+import { HTTP_STATUS } from "../../../constants/httpConstants";
 
 /**
  * Get all Pokemon
