6aa4a0c1ad0349ef292e8be0719789383045d1c4
Parth Joshi <pjoshi32@academic.RRC.CA>
2026-04-06 21:36:19 -0500

    Added authentication and authorization to routes

--- src/api/v1/routes/teamRoutes.ts
+++ src/api/v1/routes/teamRoutes.ts
@@ -2,13 +2,15 @@ import express, { Router } from "express";
 import { validateRequest } from "../middleware/validate";
 import * as teamController from "../controllers/teamController";
 import { teamSchemas } from "../validation/teamSchemas";
+import authenticate from "../middleware/authenticate";
+import isAuthorized from "../middleware/authorize";
 
 const router: Router = express.Router();
 
-router.get("/teams", teamController.getAllTeams);
-router.get("/teams/:id", validateRequest(teamSchemas.getById), teamController.getTeamById);
-router.post("/teams", validateRequest(teamSchemas.create), teamController.createTeam);
-router.put("/teams/:id", validateRequest(teamSchemas.update), teamController.updateTeam);
-router.delete("/teams/:id", validateRequest(teamSchemas.delete), teamController.deleteTeam);
+router.get("/teams", authenticate, isAuthorized({ hasRole: ["admin", "trainer"] }), teamController.getAllTeams);
+router.get("/teams/:id", authenticate, isAuthorized({ hasRole: ["admin", "trainer"], allowSameUser: true }), validateRequest(teamSchemas.getById), teamController.getTeamById);
+router.post("/teams", authenticate, isAuthorized({ hasRole: ["trainer"] }), validateRequest(teamSchemas.create), teamController.createTeam);
+router.put("/teams/:id", authenticate, isAuthorized({ hasRole: ["trainer"], allowSameUser: true }), validateRequest(teamSchemas.update), teamController.updateTeam);
+router.delete("/teams/:id", authenticate, isAuthorized({ hasRole: ["trainer"], allowSameUser: true }), validateRequest(teamSchemas.delete), teamController.deleteTeam);
 
 export default router;
